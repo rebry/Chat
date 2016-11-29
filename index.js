eed001d5b2d238b9a9b3fab2bf718a543f8001c4
rebry <rebry>
2016-11-30 00:02:22 +0100

    Feilrettinger

--- index.js
+++ index.js
@@ -27,43 +27,55 @@ en 'connection' event blir sendt fra brukeren så fort de har lastet siden,
 dette setter basisen for en socket.io samtale
  */
 io.on('connection', function (samtale) {
-    var validUser = false;                          // sjekker om brukeren har logget på.
+    var erSamtaleBrukernavnSatt = false;                          // sjekker om brukeren har logget på.
 
 
     /*
-    en 'leggTilBruker' event
+    en 'leggTilBruker' event setter samtales brukernavn til hva som ble skrevet inn på brukernavnet før en komemer til chat'en
+    Den trigger en:
+    'logget in' : Som returnerer antall brukere som er tilkoblet serveren.
+    'bruker tilkoblet' : Som forteller alle andre brukere at en ny person nå er med i samtalen.
      */
     samtale.on('leggTilBruker', function (brukernavn) {
-        if (validUser) return;
+        if (erSamtaleBrukernavnSatt) return;
         samtale.brukernavn = brukernavn;
         brukereOnline++;
-        validUser = true;
+        erSamtaleBrukernavnSatt = true;
         samtale.emit('logget in', {antallBrukere: brukereOnline});
         samtale.broadcast.emit('bruker tilkoblet', {brukernavn: samtale.brukernavn, antallBrukere: brukereOnline});
     });
 
+    /*
+    Denne funksjonen tar inn meldingen fra brukeren, og videresender den til Alle brukere, sammen med brukernavnet som sendte meldingen
+     */
     samtale.on('ny melding', function (melding) {
         samtale.broadcast.emit('ny melding', {brukernavn: samtale.brukernavn, melding: melding});
     });
 
-
+    /*
+    Om en bruker skriver noe på tastauret eller ligenden sendes det en 'skriver' event.
+    Denne funksjonen videresender dette til alle brukere, sammen med brukernavnet som skriver.
+     */
     samtale.on('skriver', function () {
         samtale.broadcast.emit('skriver', {brukernavn: samtale.brukernavn});
     });
 
+    /*
+    Denn funksjonen tar imot om brukeren har sluttet å skrive, for å si ifra dette til alle andre brukere.
+    Dette for at de andre brukerene skal få fjernet "bruker x skriver"
+     */
     samtale.on('skriver ikke', function () {
         samtale.broadcast.emit('skriver ikke', {brukernavn: samtale.brukernavn});
     });
 
     /*
     så en liten opprydding om en bruker quitter, eller mister nettet.
-    socket ser at tilkoblingen brytes, og den fjerer da brukeren så fremt validUser tilstede.
+    socket ser at tilkoblingen brytes, og den fjerer da brukeren så fremt et brukernavn er satt til samtalen.
     så fjernes 1 fra antallet slik at antall online brukere stemmer.
-    etter dette er gjort, pushes så en 'user left' til alle brukere, sammen med brukeravnet som forsvant, sammen med antall online brukere.
+    etter dette er gjort, pushes så en 'bruker frakoblet' til alle brukere, sammen med brukeravnet som forsvant, sammen med antall online brukere.
      */
-
     samtale.on('disconnect', function () {
-        if (validUser) {
+        if (erSamtaleBrukernavnSatt) {
             brukereOnline--;
             samtale.broadcast.emit('bruker frakoblet', {brukernavn: samtale.brukernavn, antallBrukere: brukereOnline });
         }
